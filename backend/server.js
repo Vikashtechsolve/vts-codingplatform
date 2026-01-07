320fefc38b19100527554bfcff4d8623dc8db306
Amitvaghamshi <amitvaghamshi9@gmail.com>
2026-01-08 00:33:43 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -15,12 +15,13 @@ app.use((req, res, next) => {
 });
 
 // Middleware
-app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002','https://vts-codingplatform.vercel.app/login'],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+// app.use(cors({
+//   origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002','https://vts-codingplatform.vercel.app/login'],
+//   credentials: true,
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization']
+// }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
